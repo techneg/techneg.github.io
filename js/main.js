c4702dc6ecbb7152dc742e3ef9c121c7a7d63542
techneg <65635458+techneg@users.noreply.github.com>
2023-04-18 08:23:26 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -39,3 +39,23 @@ VideoBtn.forEach(btn =>{
         document.querySelector('#video-slider').src = src;
     });
 });
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay:{
+        delay:2500,
+        disableOnIneraction:false,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    },
+});
