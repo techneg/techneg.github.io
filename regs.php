db851b98ca43b6a8c41e1bcf045f09503b5beb68
techneg <65635458+techneg@users.noreply.github.com>
2023-04-18 07:17:32 +0000

    updated content

---
+++ regs.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+    <script
+      src="https://kit.fontawesome.com/90489c8f29.js"
+      crossorigin="anonymous"
+    ></script>
+    <title>Register</title>
+</head>
+<body>
+<header>
+        <i class="fa-solid fa-bars" id="menu-bar"></i>
+        <a href="#" class="logo"><span>H</span>ostel</a>
+        <nav class="navbar">
+          <a href="#Home">Home</a>
+          <a href="hall.html">Halls</a>
+          <a href="gallary.html">Gallary</a>
+          <a href="booking.html">Booking</a>
+          <a href="contact.html">contact</a>
+        </nav>
+        <div class="icon">
+          <!-- <i class="fa-thin fa-square-xmark"></i> -->
+          <!-- <i class="fa-fa-search" id="search-btn"></i> -->
+          <i class="fa-solid fa-magnifying-glass" id="search-btn"></i>
+          <i class="fa-solid fa-user" id="login-btn"></i>
+          <!-- <i class="fa-fa-user" id="user-btn"></i> -->
+        </div>
+        <form action="" class="search-bar-container">
+          <input type="search" id="search-bar" placeholder="search here...." />
+          <label for="search-bar" class="fa-solid fa-magnifying-glass"></label>
+        </form>
+      </header>
+    
+</body>
+</html>
